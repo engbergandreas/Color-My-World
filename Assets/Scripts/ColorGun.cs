8410171e52d5ce19b7c3c3156757abc4472cbe3a
Andreas <adde.engberg@gmail.com>
2021-09-01 22:49:53 +0200

    Added color gun to color objects

---
+++ Assets/Scripts/ColorGun.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorGun : MonoBehaviour
+{
+    public Color color;
+    public float fireRate = 10.0f;
+    public bool continiousFire;
+
+    private Camera _cam;
+    private float fireTimer;
+
+    private void Start()
+    {
+        _cam = Camera.main;
+        fireTimer = 0;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            color = Color.red;
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            color = Color.green;
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            color = Color.blue;
+
+        if (fireTimer <= 0)
+        {
+            if (continiousFire && Input.GetMouseButton(0)) //can hold down mouse
+                FireGun();
+            else if (Input.GetMouseButtonDown(0)) //single taps only
+                FireGun();
+        }
+        else
+        {
+            fireTimer -= Time.deltaTime;
+        }
+    }
+
+    public void FireGun()
+    {
+        fireTimer = 1 / fireRate;
+
+        Vector3 mousePosition = Input.mousePosition;
+        Ray screenPosition = _cam.ScreenPointToRay(mousePosition);
+        if(!Physics.Raycast(screenPosition, out RaycastHit hitinfo, 50))
+        {
+            return;
+        }
+        GameObject hitTarget = hitinfo.transform.gameObject;
+        Vector3 hitPoint = _cam.WorldToScreenPoint(hitinfo.point);
+        hitTarget.GetComponent<ColorableObject>().ColorTarget(hitPoint, color, _cam);
+
+    }
+}
