852e3c87cb957ef48075b3ff8f9bb6d83847ee8d
Andreas <adde.engberg@gmail.com>
2021-09-27 09:54:38 +0200

    Added unsafe falling ground

---
+++ Assets/Scripts/UnsafeGround.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnsafeGround : MonoBehaviour
+{
+    public float lifeTime;
+    
+    private Vector3 ogPosition, ogScale;
+    private Quaternion ogRotation;
+    private float resetTime = 5.0f;
+
+    public float timeLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ogPosition = transform.position;
+        ogRotation = transform.rotation;
+        ogScale = transform.localScale;
+        Reset();
+    }
+
+    private void Reset()
+    {
+        transform.position = ogPosition;
+        transform.rotation = ogRotation;
+        transform.localScale = ogScale;
+        timeLeft = lifeTime;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0)
+        {
+            Fall();
+        }
+    }
+
+    void Fall()
+    {
+        transform.Translate(Vector3.down * 5);
+        OnGroundFallen();
+    }
+
+    void OnGroundFallen()
+    {
+        Invoke("Reset", resetTime);
+    }
+}
